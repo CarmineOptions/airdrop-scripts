d161c675f0ed052ddcd80610834287446c5f2f13
Andy <chepelau.a@gmail.com>
2023-06-26 14:22:31 +0200

    Add notes for 3rd airdrop

---
+++ src/drop_3.py
@@ -0,0 +1,21 @@
+
+# Traders - 312 500 - 78_125/week
+# Stakers - 312 500 - 78_125/week
+
+# Community Projects
+    # Marek's Discretion
+    # 0x047991fc342a58b8446c7265b1657aa169ce0323b275dab0a06c8961bf481b37 - 5_000 Carms - reported oracle bug
+
+    # Mods - 15_000
+    # ???
+
+    # Community Devs - 30_000
+    # '0x486deba6028c880ce3d1730a4496e4f12d7b813367d43510ea410f5ff7e3efb' : 15_000, 
+    # '0x365421f66a3fb7630ac030fb83a1db5078bfe29cc22f27f95a9978ff9ab7b6e' : 15_000, 
+    
+    # Alternative FE - 30_000 ???
+
+# Activity Allocation
+    # Voting in Proposals - 25_000
+
+# Core - half of what community gets
